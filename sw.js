2d3a382253f92c0ba7ea677037168708dc9c87d6
Deepak Sharma <43700825+Deepak2478@users.noreply.github.com>
2020-11-25 10:30:08 +0530

    sw_code.js
    test

---
+++ sw.js
@@ -0,0 +1,17 @@
+
+Register one-off sync:
+
+navigator.serviceWorker.ready.then((workerRegistration) =>{
+    return workerReistration.sync.register(‘dummySync’);
+});
+// this returns a Promise{<pending>} object
+
+
+Listen for the sync event:
+
+addEventListener(‘sync’, (e) =>{
+    if(e.tag == ‘dummySync’){
+	e.waitUntil(performBackgroundSync());
+    }
+});
+
